7e2e7f893cfd0c332405f53636255d9ad17c087f
Paulo <paulo@psproject.com.br>
2025-07-18 14:27:37 -0300

    Update midi_parser.h

--- midi_parser.h
+++ midi_parser.h
@@ -1,9 +1,21 @@
 #pragma once
 #include <stdint.h>
 
+bool parse(Parser *p,uint8_t byte,uint8_t *out,uint8_t *n);
+void flush_notes();
+bool mmc_stop(MmcBuf &buf,uint8_t b);
+
 extern uint16_t noteMask[16][8];
 extern volatile uint16_t rawMask[16][8];
 extern uint8_t g_src;
+
+static inline void set_raw(uint8_t ch, uint8_t n){
+  rawMask[ch][n >> 4] |= 1u << (n & 15);
+}
+
+static inline void clr_raw(uint8_t ch, uint8_t n){
+  rawMask[ch][n >> 4] &= ~(1u << (n & 15);
+}
 
 struct Parser {
   uint8_t st;
@@ -15,7 +27,3 @@ struct Parser {
 struct MmcBuf {
   uint8_t b[6];
 };
-
-bool parse(Parser *p,uint8_t byte,uint8_t *out,uint8_t *n);
-void flush_notes();
-bool mmc_stop(MmcBuf &buf,uint8_t b);
